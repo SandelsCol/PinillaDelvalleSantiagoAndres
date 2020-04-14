7e717a7ee4db5e6d0ce1e8779390574913f88d39
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-14 18:37:06 -0500

    Mediana

--- Preinforme10_SantiagoDelvalle.py
+++ Preinforme10_SantiagoDelvalle.py
@@ -1,10 +1,9 @@
 import numpy as np
 Presupuesto_Compañia=np.array([27834,23789,30189,30967,32501,32701,31665,17155,4614,834])
 
-Diferencia = (Presupuesto_Compañia[5] - Presupuesto_Compañia[9])
+mediana=np.median(Presupuesto_Compañia)
 
-print("La diferencia entre el año 2013 (El de mayor utilidad) y el año 2017 (El de menor utilidad) es",Diferencia)
-
+print ("La mediana es",mediana)
 
 
 
